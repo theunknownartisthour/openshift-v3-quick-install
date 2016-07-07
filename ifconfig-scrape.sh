e9ce851a3412cc5e1b886b9d9af4aec467c5c451
Alex <theunknownartisthour@gmail.com>
2016-07-06 21:23:04 -0700

    Update ifconfig-scrape.sh

--- ifconfig-scrape.sh
+++ ifconfig-scrape.sh
@@ -2,5 +2,5 @@
 INTERNAL_IP_LIST=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 export INTERNAL_IP_LIST
 INTERNAL_IP_ARRAY=(${INTERNAL_IP_LIST[@]})
-PRIMARY_INTERNAL_IP=${INTERNAL_IP_LIST[0]}
+PRIMARY_INTERNAL_IP=${INTERNAL_IP_ARRAY[0]}
 export PRIMARY_INTERNAL_IP
